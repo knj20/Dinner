7f527f73dbd1e522d55e97ee261345942a194ec0
Karima El Khayati <karimanessane@gmail.com>
2022-12-11 18:48:39 +0100

    test2

--- Dinner.Api/Program.cs
+++ Dinner.Api/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication()
                     .AddInfrastructure(builder.Configuration);
-
     builder.Services.AddControllers();
 }
 
